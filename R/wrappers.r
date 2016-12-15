94b9428789e080ec615a13df9b009717ea116f28
Drew Schmidt <wrathematics@gmail.com>
2016-12-15 18:04:50 -0500

    add help

--- R/wrappers.r
+++ R/wrappers.r
@@ -1,5 +1,15 @@
+#' Matrix Row/Col Sums
+#' 
+#' Optimized versions of R's \code{rowSums()} and \code{colSums()}.
+#' 
+#' @name rc_sums
+#' @rdname rc_sums
+NULL
+
+#' @rdname rc_sums
 #' @export
 col_sums <- function(x, na.rm=FALSE) .Call(R_col_sums, x, na.rm)
 
+#' @rdname rc_sums
 #' @export
 row_sums <- function(x, na.rm=FALSE) .Call(R_row_sums, x, na.rm)
