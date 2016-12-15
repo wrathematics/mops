a41bf182248441ea0c3496014327c423b6462b09
Drew Schmidt <wrathematics@gmail.com>
2016-12-15 17:58:21 -0500

    add na.rm option to col_sums()

--- src/col_sums.cc
+++ src/col_sums.cc
@@ -1,17 +1,37 @@
 #include "safeomp.h"
-#include "config.h"
+#include "mops.h"
+
+#include "NA.hh"
 
 template <typename T>
-static inline int col_sums(const int m, const int n, const T*const restrict x, T*const restrict ret)
+static inline int col_sums(cbool narm, cint m, cint n, const T*const restrict x, T*const restrict ret)
 {
-  #pragma omp parallel for if(m*n>OMP_MIN_SIZE)
-  for (int j=0; j<n; j++)
+  if (!narm)
   {
-    ret[j] = 0;
-    
-    SAFE_SIMD
-    for (int i=0; i<m; i++)
-      ret[j] += x[i + m*j];
+    #pragma omp parallel for if(m*n>OMP_MIN_SIZE)
+    for (int j=0; j<n; j++)
+    {
+      ret[j] = 0;
+      
+      SAFE_SIMD
+      for (int i=0; i<m; i++)
+        ret[j] += x[i + m*j];
+    }
+  }
+  else
+  {
+    #pragma omp parallel for if(m*n>OMP_MIN_SIZE)
+    for (int j=0; j<n; j++)
+    {
+      ret[j] = 0;
+      
+      SAFE_SIMD
+      for (int i=0; i<m; i++)
+      {
+        if (!check_na(x[i + m*j]))
+          ret[j] += x[i + m*j];
+      }
+    }
   }
   
   return 0;
@@ -19,12 +39,12 @@ static inline int col_sums(const int m, const int n, const T*const restrict x, T
 
 
 
-extern "C" int col_sums_int(const int m, const int n, const int*const restrict x, int *const restrict ret)
+extern "C" int col_sums_int(cbool narm, cint m, cint n, cint *const restrict x, int *const restrict ret)
 {
-  return col_sums(m, n, x, ret);
+  return col_sums(narm, m, n, x, ret);
 }
 
-extern "C" int col_sums_dbl(const int m, const int n, const double*const restrict x, double *const restrict ret)
+extern "C" int col_sums_dbl(cbool narm, cint m, cint n, cdbl *const restrict x, double *const restrict ret)
 {
-  return col_sums(m, n, x, ret);
+  return col_sums(narm, m, n, x, ret);
 }
