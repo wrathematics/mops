7f0a1f01ebc626c3c7089686e0d79aee0fe39054
Drew Schmidt <wrathematics@gmail.com>
2016-12-15 18:05:07 -0500

    add benchmark scripts

---
+++ inst/benchmarks/col_sums.r
@@ -0,0 +1,13 @@
+library(rbenchmark)
+library(mops)
+
+m = 5000
+n = 5000
+x = matrix(rnorm(m*n), m, n)
+
+cols <- cols <- c("test", "replications", "elapsed", "relative")
+
+benchmark(a <- colSums(x), b <- col_sums(x), columns=cols)
+all.equal(a, b)
+benchmark(a <- colSums(x, na.rm=TRUE), b <- col_sums(x, na.rm=TRUE), columns=cols)
+all.equal(a, b)
