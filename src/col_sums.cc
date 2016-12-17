7df11bc043d363f083ceb8396cb25d6b1b079b55
Drew Schmidt <wrathematics@gmail.com>
2016-12-16 23:48:17 -0500

    add col_means

--- src/col_sums.cc
+++ src/col_sums.cc
@@ -2,9 +2,11 @@
 #include "mops.h"
 
 #include "NA.hh"
+#include "utils.hh"
 
-template <typename T>
-static inline int col_sums_narm(cint m, cint n, const T*const restrict x, T*const restrict ret)
+
+template <typename T, typename U>
+static inline int col_sums_narm(cint m, cint n, const T *const restrict x, U *const restrict ret)
 {
   #pragma omp parallel for if(m*n>OMP_MIN_SIZE)
   for (int j=0; j<n; j++)
@@ -15,7 +17,7 @@ static inline int col_sums_narm(cint m, cint n, const T*const restrict x, T*cons
     for (int i=0; i<m; i++)
     {
       if (!check_na(x[i + m*j]))
-        ret[j] += x[i + m*j];
+        ret[j] += (U) x[i + m*j];
     }
   }
   
@@ -37,7 +39,8 @@ static inline int col_sums_dbl_nonarm(cint m, cint n, const double *const restri
   return 0;
 }
 
-static inline int col_sums_int_nonarm(cint m, cint n, const int *const restrict x, int *const restrict ret)
+template <typename T>
+static inline int col_sums_int_nonarm(cint m, cint n, const int *const restrict x, T *const restrict ret)
 {
   #pragma omp parallel for if(m*n>OMP_MIN_SIZE)
   for (int j=0; j<n; j++)
@@ -47,10 +50,10 @@ static inline int col_sums_int_nonarm(cint m, cint n, const int *const restrict 
     for (int i=0; i<m; i++)
     {
       if (!check_na(x[i + m*j]))
-        ret[j] += x[i + m*j];
+        ret[j] += (T) x[i + m*j];
       else
       {
-        ret[j] = INT32_MIN;
+        set_na(ret + j);
         break;
       }
     }
@@ -59,9 +62,35 @@ static inline int col_sums_int_nonarm(cint m, cint n, const int *const restrict 
   return 0;
 }
 
+template <typename T>
+static inline int col_means_narm(cint m, cint n, const T *const restrict x, double *const restrict ret)
+{
+  #pragma omp parallel for if(m*n>OMP_MIN_SIZE)
+  for (int j=0; j<n; j++)
+  {
+    ret[j] = 0;
+    int nobs = m;
+    
+    SAFE_SIMD
+    for (int i=0; i<m; i++)
+    {
+      if (!check_na(x[i + m*j]))
+        ret[j] += (double) x[i + m*j];
+      else
+        nobs--;
+    }
+    
+    ret[j] /= (double)nobs;
+  }
+  
+  return 0;
+}
 
 
-extern "C" int col_sums_int(cbool narm, cint m, cint n, cint *const restrict x, int *const restrict ret)
+
+extern "C" {
+
+int col_sums_int(cbool narm, cint m, cint n, cint *const restrict x, int *const restrict ret)
 {
   if (narm)
     return col_sums_narm(m, n, x, ret);
@@ -69,10 +98,34 @@ extern "C" int col_sums_int(cbool narm, cint m, cint n, cint *const restrict x, 
     return col_sums_int_nonarm(m, n, x, ret);
 }
 
-extern "C" int col_sums_dbl(cbool narm, cint m, cint n, cdbl *const restrict x, double *const restrict ret)
+int col_sums_dbl(cbool narm, cint m, cint n, cdbl *const restrict x, double *const restrict ret)
 {
   if (narm)
     return col_sums_narm(m, n, x, ret);
   else
     return col_sums_dbl_nonarm(m, n, x, ret);
+}
+
+int col_means_int(cbool narm, cint m, cint n, cint *const restrict x, double *const restrict ret)
+{
+  if (narm)
+    return col_means_narm(m, n, x, ret);
+  else
+  {
+    col_sums_int_nonarm(m, n, x, ret);
+    return vec_scale(1.0/m, n, ret);
+  }
+}
+
+int col_means_dbl(cbool narm, cint m, cint n, cdbl *const restrict x, double *const restrict ret)
+{
+  if (narm)
+    return col_means_narm(m, n, x, ret);
+  else
+  {
+    col_sums_dbl_nonarm(m, n, x, ret);
+    return vec_scale(1.0/m, n, ret);
+  }
+}
+
 }
