5e30bf679275269b8c6cdd6a501489d09ef83e55
Drew Schmidt <wrathematics@gmail.com>
2016-12-16 23:48:27 -0500

    add row_means

--- src/row_sums.cc
+++ src/row_sums.cc
@@ -1,13 +1,15 @@
+#include <stdlib.h>
 #include <string.h>
 
 #include "safeomp.h"
 #include "mops.h"
 
 #include "NA.hh"
+#include "utils.hh"
 
 
-template <typename T>
-static inline int row_sums_narm(cint m, cint n, const T *const restrict x, T *const restrict ret)
+template <typename T, typename U>
+static inline int row_sums_narm(cint m, cint n, const T *const restrict x, U *const restrict ret)
 {
   memset(ret, 0, m*sizeof(*ret));
   
@@ -17,7 +19,7 @@ static inline int row_sums_narm(cint m, cint n, const T *const restrict x, T *co
     for (int i=0; i<m; i++)
     {
       if (!check_na(x[i + m*j]))
-        ret[i] += x[i + m*j];
+        ret[i] += (U) x[i + m*j];
     }
   }
   
@@ -38,7 +40,8 @@ static inline int row_sums_dbl_nonarm(cint m, cint n, const double *const restri
   return 0;
 }
 
-static inline int row_sums_int_nonarm(cint m, cint n, const int *const restrict x, int *const restrict ret)
+template <typename T>
+static inline int row_sums_int_nonarm(cint m, cint n, const int *const restrict x, T *const restrict ret)
 {
   memset(ret, 0, m*sizeof(*ret));
   
@@ -48,18 +51,54 @@ static inline int row_sums_int_nonarm(cint m, cint n, const int *const restrict 
     for (int i=0; i<m; i++)
     {
       if (!check_na(x[i + m*j]) && !check_na(ret[i]))
-        ret[i] += x[i + m*j];
+        ret[i] += (T) x[i + m*j];
       else
-        ret[i] = INT32_MIN;
+        set_na(ret + i);
     }
   }
   
   return 0;
 }
 
+template <typename T>
+static inline int row_means_narm(cint m, cint n, const T *const restrict x, double *const restrict ret)
+{
+  int *nobs = (int*) malloc(m * sizeof(*nobs));
+  if (nobs == NULL)
+    return ALLOC_ERR;
+  
+  SAFE_FOR_SIMD
+  for (int i=0; i<m; i++)
+    nobs[i] = n;
+  
+  memset(ret, 0, m*sizeof(*ret));
+  
+  for (int j=0; j<n; j++)
+  {
+    SAFE_FOR_SIMD
+    for (int i=0; i<m; i++)
+    {
+      if (!check_na(x[i + m*j]))
+        ret[i] += (double) x[i + m*j];
+      else
+        nobs[i]--;
+    }
+  }
+  
+  SAFE_FOR_SIMD
+  for (int i=0; i<m; i++)
+    ret[i] /= (double) nobs[i];
+  
+  free(nobs);
+  
+  return 0;
+}
 
 
-extern "C" int row_sums_int(cbool narm, cint m, cint n, cint *const restrict x, int *const restrict ret)
+
+extern "C" {
+
+int row_sums_int(cbool narm, cint m, cint n, cint *const restrict x, int *const restrict ret)
 {
   if (narm)
     return row_sums_narm(m, n, x, ret);
@@ -67,10 +106,35 @@ extern "C" int row_sums_int(cbool narm, cint m, cint n, cint *const restrict x, 
     return row_sums_int_nonarm(m, n, x, ret);
 }
 
-extern "C" int row_sums_dbl(cbool narm, cint m, cint n, cdbl *const restrict x, double *const restrict ret)
+int row_sums_dbl(cbool narm, cint m, cint n, cdbl *const restrict x, double *const restrict ret)
 {
   if (narm)
     return row_sums_narm(m, n, x, ret);
   else
     return row_sums_dbl_nonarm(m, n, x, ret);
+}
+
+int row_means_int(cbool narm, cint m, cint n, cint *const restrict x, double *const restrict ret)
+{
+  if (narm)
+    return row_means_narm(m, n, x, ret);
+  else
+  {
+    row_sums_int_nonarm(m, n, x, ret);
+    return vec_scale(1.0/n, m, ret);
+  }
+  
+}
+
+int row_means_dbl(cbool narm, cint m, cint n, cdbl *const restrict x, double *const restrict ret)
+{
+  if (narm)
+    return row_means_narm(m, n, x, ret);
+  else
+  {
+    row_sums_dbl_nonarm(m, n, x, ret);
+    return vec_scale(1.0/n, m, ret);
+  }
+}
+
 }
