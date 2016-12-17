e46e37e975caa0380b3a2415fdf051b4106fce84
Drew Schmidt <wrathematics@gmail.com>
2016-12-16 23:49:15 -0500

    new tests

--- tests/row_sums.r
+++ tests/row_sums.r
@@ -1,20 +1,35 @@
 library(mops)
 set.seed(12345)
 
-test = function(x, na.rm) stopifnot(all.equal(rowSums(x, na.rm), row_sums(x, na.rm)))
+sums = function(x, na.rm) stopifnot(all.equal(rowSums(x, na.rm), row_sums(x, na.rm)))
+means = function(x, na.rm) stopifnot(all.equal(rowMeans(x, na.rm), row_means(x, na.rm)))
+
 m = 30
 n = 10
-x = matrix(rnorm(m*n), m, n)
+x = matrix(rnorm(m*n, 1000, 200), m, n)
 
-test(x, FALSE)
-test(t(x), FALSE)
+sums(x, FALSE)
+sums(t(x), FALSE)
 
-test(x, TRUE)
+means(x, FALSE)
+means(t(x), FALSE)
+
+
+sums(x, TRUE)
 x[c(1, 7, 99)] = NA
-test(x, TRUE)
-test(t(x), TRUE)
+sums(x, TRUE)
+sums(t(x), TRUE)
+
+means(x, TRUE)
+x[c(1, 7, 99)] = NA
+means(x, TRUE)
+means(t(x), TRUE)
+
 
 
 storage.mode(x) = "integer"
-test(x, FALSE)
-test(t(x), FALSE)
+sums(x, FALSE)
+sums(t(x), FALSE)
+
+means(x, FALSE)
+means(t(x), FALSE)
