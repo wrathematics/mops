91e1266859fdb36c353d48e19842135ebd73b775
Drew Schmidt <wrathematics@gmail.com>
2016-12-20 12:44:07 -0500

    add optional na.rm to row_min/max

--- src/row_minmax.cc
+++ src/row_minmax.cc
@@ -8,7 +8,7 @@
 
 
 template <typename T>
-static inline int row_mins(cint m, cint n, const T *const restrict x, T *const restrict ret)
+static inline int row_mins_narm(cint m, cint n, const T *const restrict x, T *const restrict ret)
 {
   memcpy(ret, x, m*sizeof(*ret));
   
@@ -26,7 +26,25 @@ static inline int row_mins(cint m, cint n, const T *const restrict x, T *const r
 }
 
 template <typename T>
-static inline int row_maxs(cint m, cint n, const T *const restrict x, T *const restrict ret)
+static inline int row_mins_nonarm(cint m, cint n, const T *const restrict x, T *const restrict ret)
+{
+  memcpy(ret, x, m*sizeof(*ret));
+  
+  for (int j=1; j<n; j++)
+  {
+    #pragma omp parallel for if(m>=OMP_MIN_SIZE)
+    for (int i=0; i<m; i++)
+    {
+      if (ret[i] > x[i + m*j])
+        ret[i] = x[i + m*j];
+    }
+  }
+  
+  return 0;
+}
+
+template <typename T>
+static inline int row_maxs_narm(cint m, cint n, const T *const restrict x, T *const restrict ret)
 {
   memcpy(ret, x, m*sizeof(*ret));
   
@@ -43,28 +61,58 @@ static inline int row_maxs(cint m, cint n, const T *const restrict x, T *const r
   return 0;
 }
 
+template <typename T>
+static inline int row_maxs_nonarm(cint m, cint n, const T *const restrict x, T *const restrict ret)
+{
+  memcpy(ret, x, m*sizeof(*ret));
+  
+  for (int j=1; j<n; j++)
+  {
+    #pragma omp parallel for if(m>=OMP_MIN_SIZE)
+    for (int i=0; i<m; i++)
+    {
+      if (ret[i] < x[i + m*j])
+        ret[i] = x[i + m*j];
+    }
+  }
+  
+  return 0;
+}
+
 
 
 extern "C" {
 
-int row_mins_int(cint m, cint n, cint *const restrict x, int *const restrict ret)
+int row_mins_int(cbool narm, cint m, cint n, cint *const restrict x, int *const restrict ret)
 {
-  return row_mins(m, n, x, ret);
+  if (narm)
+    return row_mins_narm(m, n, x, ret);
+  else
+    return row_mins_nonarm(m, n, x, ret);
 }
 
-int row_mins_dbl(cint m, cint n, cdbl *const restrict x, double *const restrict ret)
+int row_mins_dbl(cbool narm, cint m, cint n, cdbl *const restrict x, double *const restrict ret)
 {
-  return row_mins(m, n, x, ret);
+  if (narm)
+    return row_mins_narm(m, n, x, ret);
+  else
+    return row_mins_nonarm(m, n, x, ret);
 }
 
-int row_maxs_int(cint m, cint n, cint *const restrict x, int *const restrict ret)
+int row_maxs_int(cbool narm, cint m, cint n, cint *const restrict x, int *const restrict ret)
 {
-  return row_maxs(m, n, x, ret);
+  if (narm)
+    return row_maxs_narm(m, n, x, ret);
+  else
+    return row_maxs_nonarm(m, n, x, ret);
 }
 
-int row_maxs_dbl(cint m, cint n, cdbl *const restrict x, double *const restrict ret)
+int row_maxs_dbl(cbool narm, cint m, cint n, cdbl *const restrict x, double *const restrict ret)
 {
-  return row_maxs(m, n, x, ret);
+  if (narm)
+    return row_maxs_narm(m, n, x, ret);
+  else
+    return row_maxs_nonarm(m, n, x, ret);
 }
 
 }
